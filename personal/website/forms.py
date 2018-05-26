15106d77918e2d28fefa3e042967bff299b3455e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-05-25 21:46:28 -0400

    Realized I needed an email and title field.

--- personal/website/forms.py
+++ personal/website/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
     name = forms.CharField()
+    email = forms.CharField()
+    title = forms.CharField()
     message = forms.CharField(widget=forms.Textarea)
 
     def send_email(self):
